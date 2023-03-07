5b4495f4a647edb38a760ef1834cd22a3814d661
vikasreddy111 <vikasreddy@virinnovations.in>
2023-03-07 19:12:24 +0530

    about changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -16,13 +16,13 @@ const About = () => {
       </h1>
       <div className={styles.Container}>
         <p className={styles.Desc}>
-          Hi, I'm VIKAS REDDY, a FRONT - END DEVELOPER with 0.6 years of
+          Hi, I`m VIKAS REDDY, a FRONT - END DEVELOPER with 0.6 years of
           experience creating beautiful and functional websites. I specialize in
           creating user-friendly, responsive, and fast-loading web pages using
           modern technologies like HTML5, CSS3, and JavaScript,React.js,Next.js
-          etc. Throughout my career, I've had the opportunity to work with a
+          etc. Throughout my career, I`ve had the opportunity to work with a
           diverse range of clients, from small startups to large corporations.
-          I've contributed to the development of websites for industries . My
+          I`ve contributed to the development of websites for industries . My
           passion for web development drives me to continuously learn new skills
           and stay up-to-date with the latest trends in the industry. My work
           philosophy is centered around collaboration and communication. I
@@ -31,9 +31,9 @@ const About = () => {
           translate complex technical requirements into simple and effective
           solutions. In my free time, I enjoy exploring new technologies,
           attending web development conferences, and contributing to open-source
-          projects. When I'm not coding, you can find me hiking, reading, or
-          spending time with my family. I'm excited to hear about your project
-          and how I can help bring your vision to life. Please don't hesitate to
+          projects. When I`m not coding, you can find me hiking, reading, or
+          spending time with my family. I`m excited to hear about your project
+          and how I can help bring your vision to life. Please don`t hesitate to
           get in touch to learn more about my skills and experience or to
           discuss your next project.
         </p>
